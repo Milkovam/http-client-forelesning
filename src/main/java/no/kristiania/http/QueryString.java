b6b207df1cb22c9743c57f8777e51ab4e80d5f2e
Milkovam <makke.milkova@gmail.com>
2020-09-04 13:51:37 +0200

    Initial failing test for QueryString

---
+++ src/main/java/no/kristiania/http/QueryString.java
@@ -0,0 +1,12 @@
+package no.kristiania.http;
+
+public class QueryString {
+    /*Dette er metoden som kalles,
+    når jeg kjører new og den tar inn et parameter som jeg kaller for queryString som er string. */
+    public QueryString(String queryString) {
+    }
+
+    public String getParameter(String status) {
+        return null;
+    }
+}
